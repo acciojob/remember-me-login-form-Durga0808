f5862aa9452a21af594930efa0018daf24180b40
Durga Prasad Bejawada <117895509+Durga0808@users.noreply.github.com>
2024-06-12 11:35:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
 //your JS code here. If required.
+document.addEventListner('DOMContentLoaded', function(){
+	const existingButton= document.getElementById('existing');
+
+	if(localStorage.getItem('username')&&localStorage.getItem('password')){
+		existingButton.style.display = 'block';
+	}
+
+	document.getElementById('loginForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+        const rememberMe = document.getElementById('checkbox').checked;
+
+        if (rememberMe) {
+            localStorage.setItem('username', username);
+            localStorage.setItem('password', password);
+        } else {
+            localStorage.removeItem('username');
+            localStorage.removeItem('password');
+        }
+
+        alert(`Logged in as ${username}`);
+        
+        if (localStorage.getItem('username') && localStorage.getItem('password')) {
+            existingButton.style.display = 'block';
+        } else {
+            existingButton.style.display = 'none';
+        }
+    });
+
+    existingButton.addEventListener('click', function() {
+        const username = localStorage.getItem('username');
+        alert(`Logged in as ${username}`);
+    });
+})
